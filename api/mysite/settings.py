bd7b9fc760d2fa2b351fbf30affe8cca5ec7d4be
zyli <lizhengyi@pku.edu.cn>
2018-04-02 20:06:05 +0800

    增加了django的docker-compose部署方式;将django的数据库从sqlite改为postgresql

--- api/mysite/settings.py
+++ api/mysite/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -123,3 +127,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
