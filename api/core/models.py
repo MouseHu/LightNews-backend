3991be6977d9ad8d11e259bdc191659dbd39b96f
zyli <lizhengyi.pku@gmail.com>
2018-05-26 11:27:01 +0800

    add more docs

--- api/core/models.py
+++ api/core/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import AbstractUser
 
 # 内置的词典
 class Word(models.Model):
-    raw = models.CharField(max_length=50)
-    meaning = models.TextField()
+    raw = models.CharField(max_length=50,help_text="单词拼写")
+    meaning = models.TextField(help_text="单词释义")
 
     def __str__(self):
         return self.raw
@@ -15,9 +15,9 @@ class Word(models.Model):
 
 # 用户信息，目前暂时就相当于生词本，但还可以添加更多内容
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # 关联自带的User结构
-    glossary = models.ManyToManyField(Word, through='Wordlist')  # 生词表
-    nickname = models.CharField(max_length=50, default='John')
+    user = models.OneToOneField(User, on_delete=models.CASCADE,help_text="用户")  # 关联自带的User结构
+    glossary = models.ManyToManyField(Word, through='Wordlist',help_text="生词表")  # 生词表
+    nickname = models.CharField(max_length=50, default='John',help_text="昵称")
 
     # 可以添加更多的资料
     def __str__(self):
@@ -26,8 +26,8 @@ class UserProfile(models.Model):
 
 # 用来存储生词表和用户关联的外键表
 class Wordlist(models.Model):
-    userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)  # 和用户绑定
-    word = models.ForeignKey(Word, on_delete=models.CASCADE)  # 单词
+    userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE,help_text="用户")  # 和用户绑定
+    word = models.ForeignKey(Word, on_delete=models.CASCADE,help_text="单词")  # 单词
 
     class Meta:
         unique_together = ('userprofile', 'word')  # 唯一性
--- api/reader/models.py
+++ api/reader/models.py
@@ -3,18 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Media(models.Model):
-    name = models.CharField(max_length=100)
-    homepage = models.URLField()
+    name = models.CharField(max_length=100,help_text="媒体的名称，例如CNN/BBC/...")
+    homepage = models.URLField(help_text="媒体的主页")
     def __str__(self):
         return self.name
 
 class Article(models.Model):
-    title = models.CharField(max_length=1000)
-    content = models.TextField()
-    abstract =  models.CharField(max_length=1000,default='No abstract')
-    source = models.URLField()
-    from_media= models.ForeignKey(Media,on_delete=models.CASCADE,null=True)
-    pub_date = models.DateTimeField('date published')
+    title = models.CharField(max_length=1000,help_text="新闻标题")
+    content = models.TextField(help_text="新闻内容，文本格式")
+    abstract =  models.CharField(max_length=1000,default='No abstract',help_text="新闻的摘要，用在列表视图中")
+    source = models.URLField(help_text="新闻原文地址")
+    from_media= models.ForeignKey(Media,on_delete=models.CASCADE,null=True,help_text="新闻来源媒体")
+    pub_date = models.DateTimeField('date published',help_text="新闻时间")
 
     def __str__(self):
         return self.title
